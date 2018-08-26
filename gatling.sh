dd383b9c1edddf61a8e3f8f64fc91793cb7db678
Julio Faerman <jfaerman@gmail.com>
2018-08-26 16:28:13 +0200

    Improved Load Testing

--- gatling.sh
+++ gatling.sh
@@ -1,1 +1,4 @@
-gatling -sf . -s sducks.BasicSimulation
+#!/bin/sh
+
+GATLING_HOME="$HOME/Dev/gatling-charts-highcharts-bundle-2.3.1"
+$GATLING_HOME/bin/gatling.sh -sf . -s sducks.LoadTesting101
---
+++ user-files/simulations/sducks/LoadTesting101.scala
@@ -0,0 +1,21 @@
+package sducks 
+
+import io.gatling.core.Predef._ 
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+
+class LoadTesting101 extends Simulation {
+  val httpConf = http.baseURL("http://localhost:5000") 
+  val x = 30;
+  val scn = scenario("LoadTesting101")
+    .exec(http("static_1mb").get("/static/1mb.txt"))
+    .exec(http("fib_rec").get(s"/fib_rec?x=$x"))
+    .exec(http("fib_iter").get(s"/fib_iter?x=$x"))
+    .exec(http("fib_gen").get(s"/fib_gen?x=$x"))
+    .exec(http("fib_memo").get(s"/fib_memo?x=$x"))
+
+  setUp(
+    scn.inject(rampUsers(120) over (300 seconds))
+  ).protocols(httpConf)
+}
