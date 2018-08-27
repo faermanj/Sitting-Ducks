748657f7b574c2266a51008e6398bfed393c9de1
Julio Faerman <jfaerman@gmail.com>
2018-08-27 14:48:31 +0200

    Fixed parameter file passing

--- cfn-base-ha-vpc.sh
+++ cfn-base-ha-vpc.sh
@@ -7,6 +7,37 @@ export UNIQUE=$(date '+%H%M%S')
 export GALLERY_ID="${GALLERY_ID:-devenv-$UNIQUE}"
 export STACK_NAME="${STACK_NAME:-$GALLERY_ID-$COMPONENT_ID}"
 
+rm cfn-base-ha-vpc.json
+touch cfn-base-ha-vpc.json
+cat <<EOT >> cfn-base-ha-vpc.json
+[
+    {
+        "ParameterKey": "GalleryId",
+        "ParameterValue": "$GALLERY_ID"
+    },
+    {
+        "ParameterKey": "CidrBlock",
+        "ParameterValue": "10.0.0.0/16"
+    },
+    {
+        "ParameterKey": "SubnetPubACidr",
+        "ParameterValue": "10.0.1.0/24"
+    },
+    {
+        "ParameterKey": "SubnetPubBCidr",
+        "ParameterValue": "10.0.3.0/24"
+    },
+    {
+        "ParameterKey": "SubnetPvtACidr",
+        "ParameterValue": "10.0.2.0/24"
+    },
+    {
+        "ParameterKey": "SubnetPvtBCidr",
+        "ParameterValue": "10.0.4.0/24"
+    }
+]
+EOT
+
 aws cloudformation create-stack \
 --stack-name $STACK_NAME \
 --parameters file://./cfn-${COMPONENT_ID}.json \
