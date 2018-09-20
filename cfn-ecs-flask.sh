b2f93f1fe433740f3e2253195317cc4ee96ca7c3
Julio Faerman <jfaerman@gmail.com>
2018-09-20 12:32:18 +0200

    Initial ECS cluster

---
+++ cfn-ecs-flask.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+set -e
+
+export COMPONENT_ID="ecs-flask"
+
+export UNIQUE=$(date '+%H%M%S')
+export BUCKET_NAME=${BUCKET_NAME:-"sitting-ducks-$UNIQUE"}
+export GALLERY_ID="${GALLERY_ID:-devenv-$UNIQUE}"
+export STACK_NAME="${STACK_NAME:-$GALLERY_ID-$COMPONENT_ID}"
+export SRC_TEMPLATE="cfn-${COMPONENT_ID}.yml"
+
+aws cloudformation deploy \
+--stack-name "$STACK_NAME" \
+--parameter-overrides "GalleryId=$GALLERY_ID"  \
+--template-file $SRC_TEMPLATE \
+--capabilities CAPABILITY_IAM
+
